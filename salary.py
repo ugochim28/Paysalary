f0d4a931de0d284676096314db63e6b55ff99cf3
ugochim28 <ugochukwuuwandu28@gmail.com>
2024-09-25 19:28:40 +0200

    added paysalary

---
+++ salary.py
@@ -0,0 +1,22 @@
+
+
+# calculate the annual salary of an employee
+
+
+# promt user to enter their name
+
+# prompt user to enter their monthly salary
+
+# calculate the annual salary by multiplying monthly salary by 12
+
+# display the annual salary
+
+name = input("Please enter your full name")
+
+salary = input("Please enter your  monthly salary")
+
+salary = float(salary)
+
+threemonths_salary = salary * 3
+
+print(f"Dear {name} your annual salary is {threemonths_salary}")
